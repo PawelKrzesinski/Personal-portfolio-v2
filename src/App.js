829a21cac41c6e088b620cba54cbb2a6bb2ab2a6
Pawel <pawel.krzesinski1989@gmail.com>
2020-11-28 19:09:12 +0000

    added projects, new section-skills, updated CSS

--- src/App.js
+++ src/App.js
@@ -8,13 +8,41 @@ class App extends Component {
 				fullName: "",
 				companyName: "",
 				email: "",
-				comment: ""
+				comment: "",
+				projects: [],
+				skills:[]
+
 	}
+	
 		
 	copyrights = () => {
 		return {__html: `&copy; Copyright ${new Date().getFullYear()}, Pawel Krzesinski`}
 	}
 	
+	componentDidMount () {
+		fetch('projects.json', {
+			headers: {
+				"Content-Type" : "application/json",
+				"Accept" : "application/json"
+			}
+		}).then(res => {
+			return res.json();
+		}).then(data => {
+			this.setState({projects: data.projects})
+		})
+		fetch('skills.json', {
+			headers: {
+				"Content-Type" : "application/json",
+				"Accept" : "application/json"
+			}
+		}).then(res => {
+			return res.json();
+		}).then(data => {
+			this.setState({skills: data.skills})
+		})
+	}
+
+
 	handleSubmit = e => {
 		e.preventDefault()
 		const spinner = document.querySelector(".spinner");
@@ -43,6 +71,10 @@ class App extends Component {
 			}
 		})
 	}
+	handleChange = e => {
+		this.setState({[e.target.name]: e.target.value})
+	}
+
 
 	resetForm = () => {
 		this.setState({
@@ -78,11 +110,18 @@ class App extends Component {
 		
 	}
 
-	handleChange = e => {
-		this.setState({[e.target.name]: e.target.value})
+
+	handleProjects = () => {
+		this.state.projects.forEach(project => {
+			console.log(project)
+		})
 	}
 
+
 	render() {
+		const {projects} = this.state;
+		const {skills} = this.state;
+		console.log(skills)
 		return (
 			<div className="App" id="top">
 				<div className="navbar">
@@ -90,6 +129,7 @@ class App extends Component {
 					<button className="navbar-home"><a href="#root">HOME</a></button>
 					<button className="navbar-about"><a href="#about">ABOUT</a></button>
 					<button className="navbar-projects"><a href="#projects">PROJECTS</a></button>
+					<button className="navbar-projects"><a href="#skills">SKILLS</a></button>
 					<button className="navbar-contact"><a href="#contact">CONTACT</a></button>
 				</div>
 				<div className="section-1" id="home">
@@ -101,30 +141,81 @@ class App extends Component {
 					</div>
 				</div>
 				<div className="section-2" id="about">
-					<h2 className="section-2-text-1">Hi ! I am Pawel, i am a proactive and responsible Self-Taught Web Developer.</h2>
-					<h2 className="section-2-text-2">I am passionate about coding and always up for a challenge. You need a web app - I get it done !</h2>
-					<h2 className="section-2-text-3">I deliver beautiful, fully responsive websites for You and Your business!</h2>
+					<img src="./images/myPhoto2.jpg" alt="" className="myPhoto"/>
+					<h2 className="section-2-text-1">Hi ! I am Pawel, I am a proactive and responsible Self-Taught Web Developer.</h2>
+					<h2 className="section-2-text-2">I am passionate about coding and always up for a challenge.</h2>
+					<h2 className="section-2-text-3">I love problem solving and have great attention to details !</h2>
+					<h2 className="section-2-text-4">I deliver beautiful, fully responsive websites for You and Your business!</h2>
+					<h2 className="section-2-text-5">If you are interested in working on a project together or need a website, <a href="#contact">GET IN TOUCH</a></h2>
 				</div>
 				<div className="section-3-bg">
 					<div className="section-3" id="projects">
-						<h2>PROJECTS SECTION COMING SOON</h2>
-						{/* <h2>HERE'S SOME OF MY PROJECTS</h2>
-						<div className="project-box">
-							<img src="#" alt="" className="project-image"/>
-							<img src="#" alt="" className="project-image"/>
-							<img src="#" alt="" className="project-image"/>
-							<img src="#" alt="" className="project-image"/>
-							<img src="#" alt="" className="project-image"/>
-							<img src="#" alt="" className="project-image"/>
-							<img src="#" alt="" className="project-image"/>
-							<img src="#" alt="" className="project-image"/>
-						</div> */}
+						<p className="section-3-subsection">PROFESSIONAL PROJECTS</p>
+						<div className="projects-professional">	
+							{projects.map(project => {
+								if(project.professional){
+									return (
+										<div key={project.key}>
+											<div className="project-box">
+												<h4>{project.title}</h4>
+												<a href={project.live} target='_blank' rel='noopener noreferrer'>
+													<img src={project.image} alt="" className="project-image" />
+												</a>
+												<div className="project-image-hover">
+													<p className="project-desc">{project.description}</p>
+													<div className="project-button-box">
+														<a href={project.live} className="project-live">Project live</a>
+														<a href={project.github} className="project-sourceCode">Source Code</a>
+													</div>	
+												</div>
+											</div>
+										</div>
+									)
+								}
+							})}
+						</div>
+						<p className="section-3-subsection">HOBBY PROJECTS</p>
+						<div className="projects-hobby">
+							{projects.map(project => {
+							if(!project.professional){
+								return (
+									<div key={project.key}>
+										<div className="project-box">
+											<h4>{project.title}</h4>
+											<a href={project.live} target='_blank' rel='noopener noreferrer'>
+												<img src={project.image} alt="" className="project-image" />
+											</a>
+											<div className="project-image-hover">
+												<p className="project-desc">{project.description}</p>
+												<div className="project-button-box">
+													<a href={project.live} className="project-live">Project live</a>
+													<a href={project.github} className="project-sourceCode">Source Code</a>
+												</div>	
+											</div>
+										</div>
+									</div>
+								)
+							}
+						})}
+						</div>			
 					</div>
 				</div>
-				<div className="section-4" id="contact">
-					<h2>E-mail me:</h2>
-					<h5>krzesinskiwebsites@outlook.com</h5>
-					<h5>Or leave me a message here:</h5>
+				<div className="section-4" id="skills"><span>SKILLS:</span>
+					<h1>In my programming journey I have developed a set of skills:</h1>
+					<div className="skills-gridbox">
+						{skills.map(skill => {
+							return (
+								<div className="skill-box" key={skill.key}>
+									<h3 className="skill-title">{skill.skill}</h3>
+									<img src={skill.icon} alt="" className="skill-icon"/>
+									<p className="skill-desc">{skill.desc}</p>
+								</div>
+							)
+						})}
+					</div>
+				</div>
+				<div className="section-5" id="contact">
+					<h3>CONTACT ME</h3>
 					<div className="message-result-container" onClick={this.messageBoxFadeOut}>
 						<h2 className="message-result"> </h2>
 						<img src={spinner} alt="loading..." className='spinner'/>
@@ -143,9 +234,13 @@ class App extends Component {
 					
 				</div>
 			<div className="footer">
+				<h5 className="email">krzesinskiwebsites@outlook.com</h5>
+				<h6 className="mobile">MOBILE: 07402273196</h6>
+				<h6 className="location">EXETER, UNITED KINGDOM</h6>
 				<span className='copyrights' id='copyrights' dangerouslySetInnerHTML={this.copyrights()}></span>
 			</div>
 		</div>	
 	  );				
 }}
+
 export default App;
