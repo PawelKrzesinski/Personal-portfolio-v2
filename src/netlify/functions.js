0c02cd08b900f381cd7ca1fc469dad2f53e6436f
Pawel <pawel.krzesinski1989@gmail.com>
2020-12-16 12:19:52 +0000

    update

--- src/netlify/functions.js
+++ src/netlify/functions.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const nodemailer = require('nodemailer');
 const cors = require('cors')
-const creds = require('./config')
+const creds = require('../config')
 const serverless = require('serverless-http')
 
 const transport =  {
